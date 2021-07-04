0eb5ee2a0fe138440f2ec3fad55b777ea77d2f93
Ivan Markov <ivan.markov@gmail.com>
2021-07-04 12:49:23 +0300

    Extra branch in panic_unwind for target_family = unix, but target_os = none

--- library/panic_unwind/src/lib.rs
+++ library/panic_unwind/src/lib.rs
@@ -49,7 +49,7 @@ cfg_if::cfg_if! {
     } else if #[cfg(any(
         all(target_family = "windows", target_env = "gnu"),
         target_os = "psp",
-        target_family = "unix",
+        all(target_family = "unix", not(all(target_os = "none", target_vendor = "espressif"))),
         all(target_vendor = "fortanix", target_env = "sgx"),
     ))] {
         // Rust runtime's startup objects depend on these symbols, so make them public.
